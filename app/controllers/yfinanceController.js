6d3018b9d84582da556c139bf82c5b41d72edd09
Michael Gau <61722401+mygau91@users.noreply.github.com>
2023-10-23 12:39:00 +0800

    updated gitignore

---
+++ app/controllers/yfinanceController.js
@@ -0,0 +1,69 @@
+const { spawn } = require("child_process");
+
+// Helper function to execute Python script and get output
+const executePythonScript = (scriptPath, args) => {
+  return new Promise((resolve, reject) => {
+    const pythonProcess = spawn("python", [scriptPath, ...args]);
+    let dataString = "";
+    pythonProcess.stdout.on("data", (data) => {
+      dataString += data.toString();
+    });
+    pythonProcess.stderr.on("data", (data) => {
+      reject(data.toString());
+    });
+    pythonProcess.on("close", (code) => {
+      resolve(dataString);
+    });
+  });
+};
+
+// Connector method to get stock statistics
+const getStockStatistics = async (req, res) => {
+  const { ticker } = req.params;
+  try {
+    const output = await executePythonScript("../services/yfinance.py", [
+      "get_stock_statistics",
+      ticker,
+    ]);
+    res.json(JSON.parse(output));
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Connector method to get historical data
+const getHistoricalData = async (req, res) => {
+  const { ticker } = req.params;
+  const { period, interval } = req.query;
+  try {
+    const output = await executePythonScript("../services/yfinance.py", [
+      "get_historical_data",
+      ticker,
+      period,
+      interval,
+    ]);
+    res.json(JSON.parse(output));
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+// Connector method to get options data
+const getOptionsData = async (req, res) => {
+  const { ticker } = req.params;
+  try {
+    const output = await executePythonScript("../services/yfinance.py", [
+      "get_options_data",
+      ticker,
+    ]);
+    res.json(JSON.parse(output));
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports = {
+  getStockStatistics,
+  getHistoricalData,
+  getOptionsData,
+};
