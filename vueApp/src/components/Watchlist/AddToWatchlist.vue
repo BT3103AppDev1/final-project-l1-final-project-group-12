3049ba709da28e37f15f20cee0457dd5433136cc
ileanagoh <ileanagoh@hotmail.com>
2023-10-30 02:55:49 +0800

    added firebase func file from PortfolioPage branch

--- vueApp/src/components/Watchlist/AddToWatchlist.vue
+++ vueApp/src/components/Watchlist/AddToWatchlist.vue
@@ -1,99 +1,118 @@
 <template>
-    <div id="addToWatchlist">
-        <form id="watchlistForm">
-            <h2 class="titleOfWatchlistForm">Add to Watchlist</h2>
-
-            <input type="text" id="stock1" required="yes" placeholder="Search by company name/code">
-            <button class="add-button">
-                <img src="@/assets/star.png" alt="" class="star-icon">
-                <span class="add-text">Add</span>
-            </button>
-        </form>
-    </div>
+  <div id="addToWatchlist">
+    <form id="watchlistForm">
+      <h2 class="titleOfWatchlistForm">Add to Watchlist</h2>
+      <input
+        type="text"
+        id="stock1"
+        required="yes"
+        placeholder="Search by company name/code"
+      />
+      <button type="button" class="add-button"  @click="savetofs()">
+        <img src="@/assets/star.png" alt="" class="star-icon" />
+        <span class="add-text">Add</span>
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
-import firebaseApp from '@/firebase.js'; // npm install firebase
-import { COLLECTION_NAMES } from '@/firebaseConfig.js';
-import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { addInstrument } from "@/firebasefunc.js";
+import { COLLECTION_NAMES } from "@/firebaseConfig.js";
+import firebaseApp from "@/firebase.js";
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
 
 export default {
   data() {
     return {
-      stockName: '',
+      stockName: "",
+
+      useremail: "",
     };
   },
+  async mounted() {
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.useremail = user.email;
+      } else {
+        this.useremail = ""; // Ensure it's cleared when the user signs out
+      }
+    });
+  },
   methods: {
-    async addStock() {
-      const db = getFirestore(firebaseApp);
+    async savetofs() {
+      console.log("Saving");
+      let stock = document.getElementById("stock1").value;
 
       const stockData = {
-        name: this.stockName,
+        Stock: stock,
         // Add other data fields as needed
       };
 
-      try {
-        // Use COLLECTION_NAMES.WISHLIST to reference the "WISHLIST" collection
-        const docRef = await addDoc(collection(db, COLLECTION_NAMES.WISHLIST), stockData);
-        console.log('Stock added to Wishlist with ID: ', docRef.id);
-        // Clear the input field after successfully adding the stock
-        this.stockName = '';
-      } catch (error) {
-        console.error('Error adding stock to Wishlist: ', error);
-      }
+      await addInstrument(COLLECTION_NAMES.WISHLIST, this.useremail, stockData);
+
+      // Reset placeholder
+      document.getElementById("watchlistForm").reset();
+
+      this.$emit("added");
     },
   },
 };
 </script>
 
 <style scoped>
+/* Your styles remain the same */
+</style>
+
+<style scoped>
 .titleOfWatchlistForm {
-    font-size: 1.9vw;
+  font-size: 1.9vw;
 }
 
 #watchlistForm {
-    text-align: center;
-    background-color: white;
-    height: 15vw;
-    border-radius: 25px;
-    box-shadow: 1px 8px 10px rgba(0, 0, 0, 0.3);
-    padding-top: 7%;
+  text-align: center;
+  background-color: white;
+  height: 15vw;
+  border-radius: 25px;
+  box-shadow: 1px 8px 10px rgba(0, 0, 0, 0.3);
+  padding-top: 7%;
 }
 
 input {
-    background-color: #F3F3F3;
-    border-radius: 6px;
-    width: 80%;
-    border: none;
-    text-align: center;
-    height: 12%;
+  background-color: #f3f3f3;
+  border-radius: 6px;
+  width: 80%;
+  border: none;
+  text-align: center;
+  height: 12%;
 }
 
 input::placeholder {
-    color: #C2C1C1;
+  color: #c2c1c1;
 }
 
 .add-button {
-    background-color: #272F51;
-    color: #fff; 
-    border: none;
-    border-radius: 10px;
-    width: 50%; 
-    height: 15%; 
-    font-size: 1.2vw;
-    cursor: pointer; 
-    margin-top: 8%; 
+  background-color: #272f51;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  width: 50%;
+  height: 15%;
+  font-size: 1.2vw;
+  cursor: pointer;
+  margin-top: 8%;
 }
 
 .add-text {
-    position: relative;
-    top: -4px; 
-    font-weight: bold;
+  position: relative;
+  top: -4px;
+  font-weight: bold;
 }
 
 .star-icon {
-    width: 1.7vw;
-    height: 1.7vw; 
-    margin-right: 5px; 
+  width: 1.7vw;
+  height: 1.7vw;
+  margin-right: 5px;
 }
 </style>
