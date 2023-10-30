daa54ef2df818a64ddc1aec165d3875d4f1bb883
Michael Gau <61722401+mygau91@users.noreply.github.com>
2023-10-30 23:55:21 +0800

    Fixed access control errors

--- app/api/server.js
+++ app/api/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 // Import the routes
 const yfinanceRoutes = require("./routes/yfinanceRoutes");
 const readDataRoutes = require("./routes/readDataRoutes");
