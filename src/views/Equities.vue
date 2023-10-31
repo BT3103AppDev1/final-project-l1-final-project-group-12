a947e7327c222357184133b2b2d434219ec85ea8
janevy <janevychew@gmail.com>
2023-11-01 02:57:31 +0800

    Updated placeholder templates

--- src/views/Equities.vue
+++ src/views/Equities.vue
@@ -1,3 +1,17 @@
 <template>
-
-</template>
+    <Navigation />
+    Equities
+  </template>
+  
+  
+  
+  <script>
+  import Navigation from "@/components/Navigation.vue";
+  
+  export default {
+    name: "Equities",
+    components: {
+      Navigation,
+    },
+  };
+  </script>
