d7a50c823fa15fdf02ff5a5e48f32ea96ef53256
ileanagoh <ileanagoh@hotmail.com>
2023-10-31 14:18:40 +0800

    Update App.vue

--- vueApp/src/App.vue
+++ vueApp/src/App.vue
@@ -1,31 +1,19 @@
+<template>
+  <router-view />
+</template>
+
 <script>
-import Equities from "./views/Equities.vue"
+import Equities from "./views/Equities.vue";
 import SearchResults from "./views/SearchResults.vue";
 
-export default{
-    name: 'App',
-    components:{
-    Equities,
-    SearchResults
-}
-}
+export default {
+  name: "App",
+};
 </script>
 
-<template>
-    <div id="app">
-      <div class="market-header">
-        <router-view></router-view> <!-- This is where routed components will be displayed -->
-      </div>
-    </div>
-  </template>
-  
-  
-  
-  <style scoped>
-  /* Your scoped styles */
-  </style>
-  
 
 <style scoped>
-
+/* Your scoped styles */
 </style>
+
+<style scoped></style>
