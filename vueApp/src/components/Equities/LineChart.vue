b00f81280e1e29c77f0851ac00d425da00ef837c
ileanagoh <ileanagoh@hotmail.com>
2023-10-31 20:24:31 +0800

    Made line graph for stock price info from yfinance

---
+++ vueApp/src/components/Equities/LineChart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="chart-container">
+      <line-chart :data="chartData" :options="chartOptions"></line-chart>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['chartData', 'chartOptions'],
+  };
+  </script>
