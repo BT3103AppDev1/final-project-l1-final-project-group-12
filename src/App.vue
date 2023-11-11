33704025f482178df74d47d0c4c3f790b91d026d
Michael Gau <61722401+mygau91@users.noreply.github.com>
2023-11-11 12:02:23 +0800

    Updated tab icon

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,3 @@
-<!-- <template>
-  <router-view />
-</template>
-
-<script>
-import Equities from "./views/Equities.vue";
-import SearchResults from "./views/SearchResults.vue";
-import Watchlist from './views/Watchlist.vue';
-
-export default {
-  name: "App",
-};
-</script>
-
-
-<style scoped>
-/* Your scoped styles */
-</style> -->
 
 <template>
   <router-view />
